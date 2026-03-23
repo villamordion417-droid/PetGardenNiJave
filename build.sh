025a7c1b813cf274007c481a7cd8a6a85aa28e16
Dion <your-github-villamordion417@gmail.com>
2026-03-23 16:31:43 +0900

    Create Super User Variables

--- build.sh
+++ build.sh
@@ -7,16 +7,23 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create superuser if it doesn't exist (Smarter version)
+# Force-sync the superuser credentials
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
+import os
+
 User = get_user_model()
-username = '$DJANGO_SUPERUSER_USERNAME'
-email = '$DJANGO_SUPERUSER_EMAIL'
-password = '$DJANGO_SUPERUSER_PASSWORD'
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
-    print(f'Superuser "{username}" created.')
+    print(f'Created superuser: {username}')
 else:
-    print(f'Superuser "{username}" already exists. Skipping.')
+    user = User.objects.get(username=username)
+    user.set_password(password)
+    user.email = email
+    user.save()
+    print(f'Updated credentials for existing user: {username}')
 EOF
