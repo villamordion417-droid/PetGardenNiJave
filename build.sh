1e01609cebfea40e3a30bd6988fe15b2ff9d4a33
Dion <your-github-villamordion417@gmail.com>
2026-03-23 16:21:06 +0900

    Make superuser creation idempotent

--- build.sh
+++ build.sh
@@ -7,10 +7,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create superuser if it doesn't exist
-if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-  python manage.py createsuperuser \
-    --no-input \
-    --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
-fi
+# Create superuser if it doesn't exist (Smarter version)
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '$DJANGO_SUPERUSER_USERNAME'
+email = '$DJANGO_SUPERUSER_EMAIL'
+password = '$DJANGO_SUPERUSER_PASSWORD'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser "{username}" created.')
+else:
+    print(f'Superuser "{username}" already exists. Skipping.')
+EOF
