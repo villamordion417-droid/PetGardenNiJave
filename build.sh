6c54bb2299126a4c43f7c9d6f8e57a7bf8b0eb35
Dion <your-github-villamordion417@gmail.com>
2026-03-23 16:15:18 +0900

    Create Super User

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+fi
